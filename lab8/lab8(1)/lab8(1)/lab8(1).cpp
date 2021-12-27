41d45ae9aae5e3509d6a10a4284de6ffbc10dee3
aanddi <kuropatkinn.a.i21@gmail.com>
2021-12-28 00:03:56 +0300

    lab8

---
+++ lab8/lab8(1)/lab8(1)/lab8(1).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    int j, i, s = 0;
+
+    for (i = 2; ; i = i + 2)
+    {
+        cin >> j;
+
+        if (j > 0)
+            s = s + (i * j);
+
+        else
+            cout << s << endl;
+    }
+
+    cin.get();
+    cin.get();
+}
---
+++ lab8/lab8(2)/lab8(2)/lab8(2).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, i;
+	long double t, a, f = 0;
+
+	cout << "enter n: ";
+	cin >> n;
+
+	cout << "enter t: ";
+	cin >> t;
+
+	for (i = 0; i <= n; i++)
+	{
+		cout << "enter a: ";
+		cin >> a;
+
+		for (int g = n - i; g >= 0; g--)
+		{
+			t *= t;
+		}
+
+		f = f + a * t;
+	}
+
+	cout << "f = " << f;
+
+	cin.get();
+	cin.get();
+}
+
