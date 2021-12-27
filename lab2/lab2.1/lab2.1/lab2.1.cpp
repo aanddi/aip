a678309e33fa727c33af5303b923ce9268202bd4
aanddi <kuropatkinn.a.i21@gmail.com>
2021-12-27 23:42:17 +0300

    lab

---
+++ lab2/lab2.1/lab2.1/lab2.1.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	cout << "Hello Crimea!" << endl;
+	cout << "I am studying at the Crimean Engineering and Pedagogical University";
+	cin.get();
+}
