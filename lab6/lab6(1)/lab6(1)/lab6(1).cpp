6c680642d6184be2e6bd1eec4d5e3f8b82dad25e
aanddi <kuropatkinn.a.i21@gmail.com>
2021-12-28 00:01:38 +0300

    lab6

---
+++ lab6/lab6(1)/lab6(1)/lab6(1).cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	int code;
+	char ch;
+
+	cout << "«type characters and press Enter: ";
+	cin >> ch;
+
+	code = (int)ch;
+
+	cout << "ASCII-code is: " << code << endl;
+	cout << "the previous character is: " << char(code - 1) << endl;
+	cout << "the next character is: " << char(code + 1) << endl;
+
+	cin.get();
+	cin.get();
+}
+
---
+++ lab6/lab6(2)/lab6(2)/lab6(2).cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include <math.h> 
+using namespace std;
+
+void main()
+{
+	double a, b, c, p, s;
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Сторона a = ";
+	cin >> a;
+
+	cout << "Сторона b = ";
+	cin >> b;
+
+	cout << "Сторона c = ";
+	cin >> c;
+
+	cout << "Полупериметр p =  ";
+	cin >> p;
+
+	s = sqrt(p * (p - a) * (p - b) * (p - c));
+
+	cout << endl << "Площадь S =  " << s;
+
+	cin.get();
+	cin.get();
+}
+
