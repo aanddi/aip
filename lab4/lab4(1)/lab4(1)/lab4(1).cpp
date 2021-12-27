1974759732711fc4ce6cdd9af9db209d3f8f0507
aanddi <kuropatkinn.a.i21@gmail.com>
2021-12-27 23:59:17 +0300

    lab4

---
+++ lab4/lab4(1)/lab4(1)/lab4(1).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	char st[6];
+	char ch;
+	cout << "type characters and press Enter: ";
+	cin >> st;
+	ch = st[0];
+	st[0] = st[3];
+	st[3] = ch;
+	cout << "the result is: " << st;
+	cin.get();
+	cin.get();
+}
---
+++ lab4/lab4(2)/lab4(2)/lab4(2).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	double x, y;
+	char z;
+
+	cout << "first number: ";
+	cin >> x;
+
+	cout << "second number: ";
+	cin >> y;
+
+	cout << "operation: ";
+	cin >> z;
+
+	if (z == '+')
+	{
+		cout << "result: " << x + y;
+	}
+	else if (z == '-')
+	{
+		cout << "result: " << x - y;
+	}
+	else
+	{
+		cout << "error";
+	}
+	cin.get();
+	cin.get();
+}
